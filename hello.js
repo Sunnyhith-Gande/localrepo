1332c3edcf0bce2d609900b8c742dff64dd93298
Sunnyhith-Gande <Sunnyhith.Gande@cognizant.com>
2026-07-15 10:31:04 +0530

    TYpe script practice

---
+++ hello.js
@@ -0,0 +1,14 @@
+"use strict";
+var s1 = "You are the creator..";
+var s2 = "all the power";
+var n1 = 10;
+document.writeln(n1);
+document.writeln(s1);
+document.writeln(s1);
+var s3 = s1;
+document.writeln(s3);
+// var book:any={
+//     bookId :111,
+//     bookTitle:"anuglar",
+//     price:2000
+// }
---
+++ hello.ts
@@ -0,0 +1,15 @@
+var s1:string="You are the creator..";
+var s2:string="all the power";
+var n1:any=10;
+document.writeln(n1);
+document.writeln(s1);
+document.writeln(s1);
+
+var s3:string=s1;
+document.writeln(s3);
+
+// var book:any={
+//     bookId :111,
+//     bookTitle:"anuglar",
+//     price:2000
+// }
